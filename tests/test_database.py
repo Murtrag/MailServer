63c24395440f3118e939bc96589cea3409a9d193
murtrag <lukasz.czernik@gmail.com>
2021-02-06 15:26:42 +0100

    Added variable test #3

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,22 @@
-from database import get_connection, get_cursor, domain_name
+from database import (get_connection, get_cursor,
+ domain_name, user, password, db_name, server_name, domain_name)
 import pytest
 
+
+@pytest.mark.connection
+def test_variables():
+	assert type(user) == str
+
+	assert type(password) == str
+
+	assert type(db_name) == str
+	assert len(db_name) > 1 
+
+	assert type(server_name) == str
+	assert len(server_name) >= 2 
+
+	assert type(domain_name) == str
+	assert len(domain_name) > 2 
 
 @pytest.mark.connection
 def test_get_connection():
@@ -12,5 +28,3 @@ def test_get_cursor():
 	sql = """SELECT id, sender, title, message FROM messages where receiver=%s ORDER BY creation_date;"""
 	cursor.execute(sql, (f"test@{domain_name}",))
 	assert len(cursor.fetchall()) == 0 # @TODO Create fixture & inc this number 
-
-
